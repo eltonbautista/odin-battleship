bfaccd30dad84756af9a1f1c37a4fc090dd26380
Elton Bautista <eltonbestaha@gmail.com>
2022-04-10 06:50:59 -0600

    Wrote up a method in Player called getUniqueRandom This method is used to get a unique random number that I can pass into a gameboard's receiveAttack() method. Only the computer gameboard should be using this method as it is supposed to be the AI.

--- src/logic/factory-function-player.js
+++ src/logic/factory-function-player.js
@@ -1,14 +1,35 @@
-const Player = function playerFactoryFunction(player) {
-  const coordinatesArray = [];
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+/* eslint-disable import/prefer-default-export */
+
+export const Player = function playerFactoryFunction(player, receiveAttack) {
   // prettier-ignore
-  const fillCoordinatesArray = (function fillCoordinatesArray() {
-    for (let i = 0; i < 100; i += 1) {
-      coordinatesArray.push(i);
+  const coordinatesArray = [];
+  const numOfRandom = 100;
+
+  // Okay so getUnique is going to be used to get a random number.
+  // It also removes that random number from an array of 0-99 so that it can't be reused.
+  const getUniqueRandom = function getUniqueRandomInt() {
+    // eslint-disable-next-line wrap-iife
+
+    if (!coordinatesArray.length) {
+      for (let i = 0; i < numOfRandom; i += 1) {
+        coordinatesArray.push(i);
+      }
     }
-  }());
-  debugger;
+    const randomIndex = Math.floor(Math.random() * coordinatesArray.length);
+    const randomVal = coordinatesArray[randomIndex];
+
+    coordinatesArray.splice(randomIndex, 1);
+    return randomVal;
+  };
+  // We want to pass gameboard's receiveAttack into this function to use it in conjunction w/ getUniqueRandom.
+  //   const randomCoordinate = function randomCoordinate() {
+  //     return receiveAttack(getUniqueRandom());
+  //   };
+
   return {
     player,
+    getUniqueRandom,
   };
 };
-const player = Player('player');
