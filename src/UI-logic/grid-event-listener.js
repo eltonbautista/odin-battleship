c766f9a08a3d79975ad9986e65960cd23549db76
Elton Bautista <eltonbestaha@gmail.com>
2022-04-11 08:16:57 -0600

    Tried to add a setTimeout(), but not working

--- src/UI-logic/grid-event-listener.js
+++ src/UI-logic/grid-event-listener.js
@@ -1,11 +1,22 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable import/prefer-default-export */
-import { Gameboard } from '../function-logic/factory-function-gameboard';
+import { computerAttack } from '../function-logic/computer-attack';
 
-export const gridMouseEvents = function gridMouseEvents(grid, gameboard) {
+export const gridMouseEvents = function gridMouseEvents(
+  grid,
+  computerGameboard,
+  playerGameboard,
+  getUniqueRandom,
+  playerCellsArray
+) {
   //   const playerCellsArray = document.querySelectorAll('.player.cell');
+
   grid.addEventListener('click', (e) => {
     e.target.style.backgroundColor = 'rgb(250, 0, 50)';
-    // console.log(e.target.dataset.computer);
-    gameboard.receiveAttack(e.target.dataset.computer);
+    computerGameboard.receiveAttack(e.target.dataset.computer);
+    setTimeout(
+      computerAttack(playerGameboard, getUniqueRandom, playerCellsArray),
+      5000
+    );
   });
 };
