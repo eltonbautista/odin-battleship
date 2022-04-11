d3bf1e88022922c59e780a7ecb5d814243a7b2eb
Elton Bautista <eltonbestaha@gmail.com>
2022-04-11 07:11:32 -0600

    moved renderGrid() to a separate module

---
+++ src/UI-logic/render-grid.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+export const renderGrid = function renderGrid(gridSize, gridIdentifier) {
+  const mainContent = document.querySelector('#main-content');
+  const gridContainer = document.createElement('div');
+  const gridCells = [];
+  const populateGridContainer = function populateGridContainer() {
+    for (let i = 0; i < gridSize; i += 1) {
+      gridCells.push(document.createElement('div'));
+      gridCells[i].setAttribute('class', `${gridIdentifier} cell`);
+      gridContainer.append(gridCells[i]);
+    }
+  };
+  populateGridContainer();
+  mainContent.append(gridContainer);
+  return gridContainer;
+};
