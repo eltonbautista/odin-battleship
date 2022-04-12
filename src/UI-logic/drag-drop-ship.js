0914be2c8657e412c19a20577b78267691c74348
Elton Bautista <eltonbestaha@gmail.com>
2022-04-12 12:24:03 -0600

    Drag drop working only as a visual tool I think this is the best way to approach this feature. It allows UI to be separated from the logic. Now, we just have to pass the appropriate thhings onto the drag and drop events.

--- src/UI-logic/drag-drop-ship.js
+++ src/UI-logic/drag-drop-ship.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable arrow-parens */
 /* eslint-disable no-console */
 /* eslint-disable no-restricted-syntax */
@@ -10,6 +11,7 @@ export const dragDropShip = function dragDropShip() {
     // eslint-disable-next-line no-loop-func
     ship.addEventListener('dragstart', (e) => {
       e.dataTransfer.setData('text/plain', ship.id);
+      console.log(e.dataTransfer.items);
     });
   }
 
@@ -18,31 +20,64 @@ export const dragDropShip = function dragDropShip() {
     '#main-content > div:first-child > div'
   );
 
+  // Okay so basically the user will drag a ship onto one of the cells, but there needs to be enough
+  // adjacent cells beside the cell it's dropped on, so that it will properly drop.
+
+  const changeBackgroundColor = function changeBackgroundColor(color, ...cell) {
+    console.log(cell);
+    // let parsedInt;
+    cell.forEach((int) => {
+      playerCellsArray[int].style.backgroundColor = `${color}`;
+    });
+  };
+
+  const stringToNumber = function stringToNumber(num) {
+    return parseInt(num);
+  };
+
   for (const cell of playerCellsArray) {
     cell.addEventListener('dragover', (e) => {
       e.preventDefault();
-      // cell.classList.add('drop-zone--over');
+      cell.classList.add('player--over');
+      changeBackgroundColor(
+        'green',
+        stringToNumber(e.target.dataset.player),
+        stringToNumber(e.target.dataset.player) + 1
+      );
+    });
+
+    cell.addEventListener('dragleave', (e) => {
+      changeBackgroundColor(
+        'blue',
+        stringToNumber(e.target.dataset.player),
+        stringToNumber(e.target.dataset.player) + 1
+      );
     });
 
     cell.addEventListener('drop', (e) => {
       e.preventDefault();
       const shipId = e.dataTransfer.getData('text/plain');
       const droppedShip = document.getElementById(shipId);
-      console.log(e);
+      console.log(e.target.dataset.player);
+      changeBackgroundColor(
+        'green',
+        stringToNumber(e.target.dataset.player),
+        stringToNumber(e.target.dataset.player) + 1
+      );
     });
   }
 
-  dropGrid.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    // dropGrid.classList.add('drop-zone--over');
-  });
+  //   dropGrid.addEventListener('dragover', (e) => {
+  //     e.preventDefault();
+  //     // dropGrid.classList.add('drop-zone--over');
+  //   });
 
-  dropGrid.addEventListener('drop', (e) => {
-    e.preventDefault();
-    const shipId = e.dataTransfer.getData('text/plain');
-    console.log(shipId);
-    const droppedShip = document.getElementById(shipId);
-    dropGrid.appendChild(droppedShip);
-    console.log(e);
-  });
+  //   dropGrid.addEventListener('drop', (e) => {
+  //     e.preventDefault();
+  //     const shipId = e.dataTransfer.getData('text/plain');
+  //     console.log(shipId);
+  //     const droppedShip = document.getElementById(shipId);
+  //     dropGrid.appendChild(droppedShip);
+  //     console.log(e);
+  //   });
 };
