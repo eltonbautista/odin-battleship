1ecb80623464b401cb3979fb85352fb258416d1b
Elton Bautista <eltonbestaha@gmail.com>
2022-04-15 07:23:15 -0600

    Refactored the logic Ships were being placed on indices that were already taken, which messed up the placing of some indices.

--- src/function-logic/coordinates-module-for-Player.js
+++ src/function-logic/coordinates-module-for-Player.js
@@ -9,29 +9,26 @@ export const coordinatesGenerator = function coordinatesGenerator(
 ) {
   const shipBeingPlaced = computerGameboard.myShips;
   const computerGameboardArray = computerGameboard.gameboardArray;
-  const firstHalf = [];
-  const secondHalf = [];
+  // const firstHalf = [];
+  // const secondHalf = [];
   const oneArray = [];
   const fillArray = function fillArray(arr, length, from) {
     for (let i = from; i < length; i += 1) {
       arr.push(i);
     }
   };
-  fillArray(firstHalf, 50, 0);
-  fillArray(secondHalf, 100, 50);
   fillArray(oneArray, 100, 0);
+  const upperHalfOfOneArray = oneArray.filter((x) => x > 50);
 
   const placeComputerShips = function placeComputerShips(
     shipIndex,
     uNum,
     iterator,
-    arr,
-    half
+    arr
   ) {
     const coords = [];
     let realNum = uNum;
     for (let i = 0; i < shipBeingPlaced[shipIndex].shipArray.length; i += 1) {
-      half.splice(half.indexOf(realNum));
       coords.push(arr.splice(arr.indexOf(realNum), 1));
       console.log(realNum);
       realNum += iterator;
@@ -43,23 +40,23 @@ export const coordinatesGenerator = function coordinatesGenerator(
       ...coords
     );
   };
-
+  // prettier-ignore
   for (let j = 0; j < shipBeingPlaced.length; j += 1) {
     // const uniqueNum = oneArray[Math.floor(Math.random() * oneArray.length)]; // Chooses a random number.
 
     if (shipBeingPlaced[j].shipArray.length > 3) {
-      const uniqueNum =
-        firstHalf[Math.floor(Math.random() * Math.ceil(firstHalf.length / 2))];
-      placeComputerShips(j, uniqueNum, 10, oneArray, firstHalf);
+      const uniqueNum = oneArray[Math.floor(Math.random() * Math.ceil(oneArray.length / 10))];
+      placeComputerShips(j, uniqueNum, 10, oneArray);
     } else if (shipBeingPlaced[j].shipArray.length <= 3) {
-      const uniqueNum =
-        secondHalf[Math.floor(Math.random() * Math.ceil(secondHalf.length))];
+      const uniqueNum = upperHalfOfOneArray[
+        Math.floor(Math.random() * Math.ceil(upperHalfOfOneArray.length))
+      ];
       if ((uniqueNum % 10) + shipBeingPlaced[j].shipArray.length > 10) {
         const useThisNum = uniqueNum - shipBeingPlaced[j].shipArray.length;
-        placeComputerShips(j, useThisNum, 1, oneArray, secondHalf);
+        placeComputerShips(j, useThisNum, 1, oneArray);
       } else {
         const useThisNum = uniqueNum;
-        placeComputerShips(j, useThisNum, 1, oneArray, secondHalf);
+        placeComputerShips(j, useThisNum, 1, oneArray);
       }
     }
   }
